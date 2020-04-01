9bbc2126dff07523650cc1a890a384eac35d8373
Blues <holyblues@gmail.com>
2020-04-02 07:45:57 +0800

    refactor(GildedRose): pass all brie item test

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -92,6 +92,14 @@ final class GildedRose
     {
         $item->sell_in -= 1;
         $item->quality += 1;
+
+        if ($item->sell_in < 0) {
+            $item->quality += 1;
+        }
+
+        if ($item->quality > 50) {
+            $item->quality = 50;
+        }
     }
 }
 
