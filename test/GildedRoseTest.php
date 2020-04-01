38660a2dadeaf81367b10436536ad9c92591a3eb
Blues <holyblues@gmail.com>
2020-04-02 06:38:49 +0800

    test(GildedRoseTest): add 'Aged Brie' item test

--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -34,7 +34,7 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(13, $items[0]->quality);
     }
 
-    function test_updates_normal_items_with_a_quility_0()
+    function test_updates_normal_items_with_a_quality_0()
     {
         $items = [new Item("normal", 1, 0)];
         $gildedRose = new GildedRose($items);
@@ -43,4 +43,58 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase
         $this->assertEquals(0, $items[0]->sell_in);
         $this->assertEquals(0, $items[0]->quality);
     }
+
+    /**
+     * brie test start
+     */
+    function test_updates_brie_items_before_the_sell_date()
+    {
+        $items = [new Item("Aged Brie", 10, 15)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals("Aged Brie", $items[0]->name);
+        $this->assertEquals(9, $items[0]->sell_in);
+        $this->assertEquals(16, $items[0]->quality);
+    }
+
+    function test_updates_brie_items_on_the_sell_date()
+    {
+        $items = [new Item("Aged Brie", 0, 15)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals("Aged Brie", $items[0]->name);
+        $this->assertEquals(-1, $items[0]->sell_in);
+        $this->assertEquals(17, $items[0]->quality);
+    }
+
+    function test_updates_brie_items_after_the_sell_date()
+    {
+        $items = [new Item("Aged Brie", -1, 15)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals("Aged Brie", $items[0]->name);
+        $this->assertEquals(-2, $items[0]->sell_in);
+        $this->assertEquals(17, $items[0]->quality);
+    }
+
+    function test_updates_brie_items_with_a_maxima_quality()
+    {
+        $items = [new Item("Aged Brie", 1, 50)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals("Aged Brie", $items[0]->name);
+        $this->assertEquals(0, $items[0]->sell_in);
+        $this->assertEquals(50, $items[0]->quality);
+    }
+
+    function test_updates_brie_items_after_the_sell_date_near_a_maxima_quality()
+    {
+        $items = [new Item("Aged Brie", 0, 49)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals("Aged Brie", $items[0]->name);
+        $this->assertEquals(-1, $items[0]->sell_in);
+        $this->assertEquals(50, $items[0]->quality);
+    }
+
 }
