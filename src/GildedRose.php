572cc0ba65eff0752f886a69a3856edf4461db17
Blues <holyblues@gmail.com>
2020-04-02 07:31:18 +0800

    refactor(GildedRose): pass the test_updates_normal_items_on_the_sell_date() and test_updates_normal_items_after_the_sell_date()

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -73,6 +73,10 @@ final class GildedRose
     {
         $item->sell_in -= 1;
         $item->quality -= 1;
+
+        if ($item->sell_in < 0) {
+            $item->quality -= 1;
+        }
     }
 }
 
