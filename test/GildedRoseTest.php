caceabc7ac96db3cccbfad03fb9f4d0662a81b8b
Blues <holyblues@gmail.com>
2020-04-02 06:52:13 +0800

    feat(GildedRoseTest): add "Sulfuras, Hand of Ragnaros" test
    add "Sulfuras, Hand of Ragnaros" test
    and remote the test of item's
    name

--- test/GildedRoseTest.php
+++ test/GildedRoseTest.php
@@ -19,7 +19,6 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase
         $items = [new Item("normal", 0, 15)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals("normal", $items[0]->name);
         $this->assertEquals(-1, $items[0]->sell_in);
         $this->assertEquals(13, $items[0]->quality);
     }
@@ -29,7 +28,6 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase
         $items = [new Item("normal", -1, 15)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals("normal", $items[0]->name);
         $this->assertEquals(-2, $items[0]->sell_in);
         $this->assertEquals(13, $items[0]->quality);
     }
@@ -39,7 +37,6 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase
         $items = [new Item("normal", 1, 0)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals("normal", $items[0]->name);
         $this->assertEquals(0, $items[0]->sell_in);
         $this->assertEquals(0, $items[0]->quality);
     }
@@ -52,7 +49,6 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase
         $items = [new Item("Aged Brie", 10, 15)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals("Aged Brie", $items[0]->name);
         $this->assertEquals(9, $items[0]->sell_in);
         $this->assertEquals(16, $items[0]->quality);
     }
@@ -62,7 +58,6 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase
         $items = [new Item("Aged Brie", 0, 15)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals("Aged Brie", $items[0]->name);
         $this->assertEquals(-1, $items[0]->sell_in);
         $this->assertEquals(17, $items[0]->quality);
     }
@@ -72,7 +67,6 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase
         $items = [new Item("Aged Brie", -1, 15)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals("Aged Brie", $items[0]->name);
         $this->assertEquals(-2, $items[0]->sell_in);
         $this->assertEquals(17, $items[0]->quality);
     }
@@ -82,7 +76,6 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase
         $items = [new Item("Aged Brie", 1, 50)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals("Aged Brie", $items[0]->name);
         $this->assertEquals(0, $items[0]->sell_in);
         $this->assertEquals(50, $items[0]->quality);
     }
@@ -92,9 +85,46 @@ class GildedRoseTest extends \PHPUnit\Framework\TestCase
         $items = [new Item("Aged Brie", 0, 49)];
         $gildedRose = new GildedRose($items);
         $gildedRose->updateQuality();
-        $this->assertEquals("Aged Brie", $items[0]->name);
         $this->assertEquals(-1, $items[0]->sell_in);
         $this->assertEquals(50, $items[0]->quality);
     }
 
+    /**
+     * Sulfuras test start
+     */
+    function test_updates_sulfuras_items_before_the_sell_date()
+    {
+        $items = [new Item("Sulfuras, Hand of Ragnaros", 10, 15)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(10, $items[0]->sell_in);
+        $this->assertEquals(15, $items[0]->quality);
+    }
+
+    function test_updates_sulfuras_items_on_the_sell_date()
+    {
+        $items = [new Item("Sulfuras, Hand of Ragnaros", 0, 15)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(0, $items[0]->sell_in);
+        $this->assertEquals(15, $items[0]->quality);
+    }
+
+    function test_updates_sulfuras_items_after_the_sell_date()
+    {
+        $items = [new Item("Sulfuras, Hand of Ragnaros", -1, 15)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(-1, $items[0]->sell_in);
+        $this->assertEquals(15, $items[0]->quality);
+    }
+
+    function test_updates_sulfuras_items_with_a_quality_0()
+    {
+        $items = [new Item("Sulfuras, Hand of Ragnaros", 1, 0)];
+        $gildedRose = new GildedRose($items);
+        $gildedRose->updateQuality();
+        $this->assertEquals(1, $items[0]->sell_in);
+        $this->assertEquals(0, $items[0]->quality);
+    }
 }
