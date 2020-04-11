7aea1141d5b3b15376a4cdc715d803bf75ad0444
Blues <holyblues@gmail.com>
2020-04-11 15:42:30 +0800

    refactor(GildedRose): add new class ConjuredItem

--- src/GildedRose.php
+++ src/GildedRose.php
@@ -18,32 +18,7 @@ final class GildedRose
     public function updateQuality()
     {
         foreach ($this->items as $item) {
-            switch ($item->name) {
-                case 'Sulfuras, Hand of Ragnaros':
-                case 'Backstage passes to a TAFKAL80ETC concert':
-                case 'Aged Brie':
-                    $item->updateQuality();
-                    break;
-                case 'Conjured':
-                    $this->conjuredQualityUpdate($item);
-                    break;
-                default:
-                    $item->updateQuality();
-            }
-        }
-    }
-
-    private function conjuredQualityUpdate(Item $item)
-    {
-        $item->sell_in -= 1;
-        $item->quality -= 2;
-
-        if ($item->sell_in < 0) {
-            $item->quality -= 2;
-        }
-
-        if ($item->quality < 0) {
-            $item->quality = 0;
+            $item->updateQuality();
         }
     }
 }
